d27b73fd2917ec17fb1810bc26aa481573acc847
Joe <joeholley@me.com>
2020-01-26 19:54:30 +0000

    Initial commit

---
+++ public/assets/js/home.js
@@ -0,0 +1,36 @@
+const $setLogin = $('#login');
+const $setSignUp = $('#signup');
+const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+
+let authSetting = 'login';
+
+function setAuth(setting) {
+  authSetting = setting;
+  
+  if (authSetting === 'login') {
+    $setLogin.addClass('active');
+    $setSignUp.removeClass('active');
+    $submitButton.text('Log In');
+  } else {
+    $setSignUp.addClass('active');
+    $setLogin.removeClass('active');
+    $submitButton.text('Sign Up');
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  console.log(
+    `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
+  );
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
